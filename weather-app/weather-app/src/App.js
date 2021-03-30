b1241146629b39a5cb995f485d4a3ea0055dd434
harry2394 <78731992+harry2394@users.noreply.github.com>
2021-03-30 15:22:34 +0100

    Add files via upload
    added weather icon summary, sunrise and sunset

---
+++ weather-app/weather-app/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import Forecast from "./Components/Forcast/Forecast";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 class="text-3xl font-extrabold text-white-900 sm:text-4xl">
+          Weather App
+        </h1>
+      </header>
+
+      <main>
+        <Forecast />
+      </main>
+
+      <footer>created by Harry</footer>
+    </div>
+  );
+}
+
+export default App;
